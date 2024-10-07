80ad4c6188d473faf019c76a5ea7dd53388d72b7
hellencarolin <159459794+hellencarolin@users.noreply.github.com>
2024-10-07 12:19:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Maquiagem',
+    'Corretivo antes ou depois da base?',
+    'Cada pessoa, profissional, ou não, tem sua maneira de preparar a pele.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Qual a melhor cor de blush para minha pele?',
+    'O tom de blush tem a ver com a tonalidade da sua pele.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Como evitar que a maquiagem craquele?',
+    'Você deve higienizar, tonificar e hidratar a pele antes da maquiagem.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Como amenizar os poros dilatados?',
+    'Utilize sempre um creme hidratante que promova o fechamento dos poros.'
+)
